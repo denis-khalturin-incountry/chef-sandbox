fdc39b5bbec0db4d7ffe234901caf770fed9fb2f
Denis Khalturin <denis.khalturin@incountry.com>
2021-12-14 00:51:20 +0300

    update

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,8 @@
 
 echo -e "y\n" | ssh-keygen -t rsa -f /tmp/.chef-sshkey -q -N "" > /dev/null
 
-SSH_KEY=$(cat /tmp/.chef-sshkey.pub)
+SSH_KEY=$(cat /tmp/.chef-sshkey)
+SSH_KEY_PUB=$(cat /tmp/.chef-sshkey.pub)
 
 find nodes -type f -name "*.json" | while read json; do
     ip=$(basename ${json} ".json")
@@ -15,8 +16,11 @@ find nodes -type f -name "*.json" | while read json; do
         -o UserKnownHostsFile=/dev/null \
         -o StrictHostKeychecking=no \
         /bin/bash
-cat >> ~/.ssh/authorized_keys <<KEY
+cat >> ~/.ssh/id_rsa <<KEY
 ${SSH_KEY} 
+KEY
+cat >> ~/.ssh/authorized_keys <<KEY
+${SSH_KEY_PUB} 
 KEY
 command -v chef-solo || curl https://www.chef.io/chef/install.sh -L | sudo bash
 rm -rf chef-sandbox
