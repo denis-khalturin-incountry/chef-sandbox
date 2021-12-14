98bd5c4ccba99099850eaf5d35f70baff01bf8bf
Denis Khalturin <denis.khalturin@incountry.com>
2021-12-15 01:33:11 +0300

    update

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ test ! -f /tmp/.chef-sshkey && echo -e "y\n" | ssh-keygen -f /tmp/.chef-sshkey -
 SSH_KEY=$(cat /tmp/.chef-sshkey)
 SSH_KEY_PUB=$(cat /tmp/.chef-sshkey.pub)
 
-find data_bags -type f -name "*backend*1.json" | while read data_bag; do
+find data_bags -type f -name "*.json" | while read data_bag; do
     json_attribs="nodes/$(basename ${data_bag})"
     ip=$(jq -r '.ip' ${data_bag})
 
@@ -14,6 +14,10 @@ find data_bags -type f -name "*backend*1.json" | while read data_bag; do
         -o UserKnownHostsFile=/dev/null \
         -o StrictHostKeychecking=no \
         /bin/bash
+install -m 400 <(cat <<KEY
+${SSH_KEY}
+KEY
+) /root/.ssh/id_rsa
 cat <<KEY | sudo tee /root/.ssh/id_rsa > /dev/null
 ${SSH_KEY}
 KEY
