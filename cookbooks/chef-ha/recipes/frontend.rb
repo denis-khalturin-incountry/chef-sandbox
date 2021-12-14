86059c045a3d36135d5cca3d57f11cd22ceafc6d
Denis Khalturin <denis.khalturin@incountry.com>
2021-12-15 02:43:01 +0300

    update

--- cookbooks/chef-ha/recipes/frontend.rb
+++ cookbooks/chef-ha/recipes/frontend.rb
@@ -1,4 +1,8 @@
-log 'message' do
-  message  'Hello Chef'
-  level    :info
+leader = {}
+data = data_bag_item('backend', node[:hostname])
+
+ruby_block 'wait-chef-frontend-config' do
+  block do
+    true until ::File.exists?("/tmp/chef-#{node[:hostname]}.rb")
+  end
 end
