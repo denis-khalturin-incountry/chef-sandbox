bb3a82831d89134959377116a802bb61f8461d93
Denis Khalturin <denis.khalturin@incountry.com>
2021-12-14 22:51:03 +0300

    update

--- cookbooks/chef-ha/recipes/backend.rb
+++ cookbooks/chef-ha/recipes/backend.rb
@@ -30,7 +30,7 @@ bash 'cluster-status' do
   # code 'chef-backend-ctl cluster-status'
   ignore_failure :quiet
 
-  # notifies :run, 'log[message]', :immediate
+  notifies :run, 'log[message]', :immediate
   # only_if { data['leader'] != true }
 end
 
