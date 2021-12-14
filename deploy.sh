4048990392c1fbfa75ccc4dcca253bdeef668532
Denis Khalturin <denis.khalturin@incountry.com>
2021-12-15 01:25:55 +0300

    update

--- deploy.sh
+++ deploy.sh
@@ -14,10 +14,10 @@ find data_bags -type f -name "*backend*1.json" | while read data_bag; do
         -o UserKnownHostsFile=/dev/null \
         -o StrictHostKeychecking=no \
         /bin/bash
-cat <<KEY | sudo tee /root/.ssh/id_rsa
+cat <<KEY | sudo tee /root/.ssh/id_rsa > /dev/null
 ${SSH_KEY}
 KEY
-cat <<KEY | sudo tee -a /root/.ssh/authorized_keys
+cat <<KEY | sudo tee -a /root/.ssh/authorized_keys > /dev/null
 ${SSH_KEY_PUB}
 KEY
 # command -v chef-solo > /dev/null || curl https://www.chef.io/chef/install.sh -L | sudo bash
