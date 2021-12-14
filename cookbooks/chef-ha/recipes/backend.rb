e0bb59c5b6588fa79e42bc9b2a5e11c183f9b8d7
Denis Khalturin <denis.khalturin@incountry.com>
2021-12-14 18:20:02 +0300

    update

--- cookbooks/chef-ha/recipes/backend.rb
+++ cookbooks/chef-ha/recipes/backend.rb
@@ -4,11 +4,11 @@ log 'message' do
 end
 
 log 'message' do
-  message  "databag:\n#{data_bag('nodes')}"
+  message  "databag:\nBACKEND:#{data_bag('backend')}\nFRONTEND:#{data_bag('frontend')}"
   level    :info
 end
 
-n = data_bag_item('nodes', 'chef-backend-02')
+n = data_bag_item('backend', 'chef-backend-02')
 
 log 'message' do
   message  "databag:\nid: #{n['id']}; leader: #{n['leader'] === true}; qqq: #{n['qqq']}"
--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,11 @@ echo -e "y\n" | ssh-keygen -t rsa -f /tmp/.chef-sshkey -q -N "" > /dev/null
 SSH_KEY=$(cat /tmp/.chef-sshkey)
 SSH_KEY_PUB=$(cat /tmp/.chef-sshkey.pub)
 
-find data_bags -type f -name "*backend*01.json" | while read json; do
-    json_attribs="nodes/$(basename ${json})"
-    ip=$(jq -r '.ip' ${json})
+find data_bags -type f -name "*backend*01.json" | while read data_bag; do
+    json_attribs="nodes/$(basename ${data_bag})"
+    ip=$(jq -r '.ip' ${data_bag})
 
-    echo ${ip}
+    echo -e "data bag:\t${data_bag}\njson attr:\t${json_attribs}\nip:\t\t${ip}"
     cat <<EOF | ssh ${ip} -l ubuntu \
         -o UserKnownHostsFile=/dev/null \
         -o StrictHostKeychecking=no \
@@ -20,7 +20,7 @@ KEY
 cat >> ~/.ssh/authorized_keys <<KEY
 ${SSH_KEY_PUB} 
 KEY
-command -v chef-solo || curl https://www.chef.io/chef/install.sh -L | sudo bash
+command -v chef-solo > /dev/null || curl https://www.chef.io/chef/install.sh -L | sudo bash
 rm -rf chef-sandbox
 git clone https://github.com/denis-khalturin-incountry/chef-sandbox
 cd chef-sandbox
