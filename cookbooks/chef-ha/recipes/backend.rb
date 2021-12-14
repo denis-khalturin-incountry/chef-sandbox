86d97baa6d50d2f131a103b54ba5c233dee45386
Denis Khalturin <denis.khalturin@incountry.com>
2021-12-14 23:32:24 +0300

    update

--- cookbooks/chef-ha/recipes/backend.rb
+++ cookbooks/chef-ha/recipes/backend.rb
@@ -19,53 +19,29 @@ file '/etc/chef-backend/chef-backend.rb' do
   content "publish_address '#{node['ipaddress']}'"
 end
 
-# file '/tmp/foo.txt' do
-#   content 'fooss'
-#   action :nothing
-#   subscribes :create, [ 'file[/tmp/bar.txt]', 'file[/tmp/baz.txt]' ]
-# end
-# file '/tmp/bar.txt' do
-#   content 'barse'
-# end
-# file '/tmp/baz.txt' do
-#   content 'baze'
-# end
-
 bash 'cluster-status' do
-  # code 'w'
   code 'chef-backend-ctl cluster-status'
   returns 1
   ignore_failure :quiet
 
-  # only_if { data['leader'] != true }
+  only_if { data['leader'] != true }
 end
 
 log 'message' do
-  message  "TEST"
-  level    :info
+  message "TEST"
+  level :info
   action :nothing
   subscribes :write, [ 'bash[cluster-status]' ]
 end
 
-log 'message' do
-  message  "TEST 2"
-  level    :info
+data_bag('backend').each do |host|
+  log 'message' do
+    message "HOST: #{host}"
+    level :info
+    action :nothing
+    subscribes :write, [ 'bash[cluster-status]' ]
+  end
 end
 
 # if data['leader'] === true
-#   log 'message' do
-#     message  shell_out("")
-#     level    :info
-#   end
-#   # bash 'create-cluster' do
-#   #   code 'chef-backend-ctl create-cluster --accept-license'
-#   #   # only_if { data['leader'] === true }
-#   # end
-
-#   # data_bag('backend').each do |host|
-#   #   log 'message' do
-#   #     message  "HOST: #{host}"
-#   #     level    :info
-#   #   end
-#   # end
 # end
