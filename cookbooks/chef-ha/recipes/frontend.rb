1559e6ffbc58b2bc08448933cc200224663ce279
Denis Khalturin <denis.khalturin@incountry.com>
2021-12-15 02:47:52 +0300

    update

--- cookbooks/chef-ha/recipes/frontend.rb
+++ cookbooks/chef-ha/recipes/frontend.rb
@@ -6,3 +6,12 @@ ruby_block 'wait-chef-frontend-config' do
     true until ::File.exists?("/tmp/chef-#{node[:hostname]}.rb")
   end
 end
+
+file "/etc/opscode/chef-server.rb`" do
+  content ::File.open("/tmp/chef-#{node[:hostname]}.rb").read
+  action :create
+end
+
+bash 'chef-server-reconfigure' do
+  code 'chef-server-ctl reconfigure'
+end
