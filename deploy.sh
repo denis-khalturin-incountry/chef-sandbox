a97c36d7aacf9c50d95ab63038cecf0bbb9678a2
Denis Halturin <dhalturin@hotmail.com>
2021-12-18 01:55:33 +0300

    update

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ command -v chef-solo > /dev/null || curl https://www.chef.io/chef/install.sh -L 
 rm -rf chef-sandbox
 git clone https://github.com/denis-khalturin-incountry/chef-sandbox
 cd chef-sandbox
-chef-solo --chef-license accept -c solo.rb -o 'recipe[chef-ha::frontend]'
+chef-solo --chef-license accept -c solo.rb -o 'recipe[chef-ha::backend]'
 EOF
